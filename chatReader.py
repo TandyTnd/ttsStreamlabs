a9567f4c29761e52c34a1b71a16a3f2a7a2ee98b
Oscar Zuñiga Lara <30669700+OscarZunigaLara@users.noreply.github.com>
2023-03-13 18:30:25 -0600

    first

---
+++ chatReader.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from timeit import default_timer
+import time
+import pyttsx3
+
+import simpleaudio as sa
+
+
+url = "https://streamlabs.com/widgets/chat-box/v1/3B42D72ED1F8E3C39A83"
+resetComment = 30
+
+
+from selenium import webdriver
+# Set up the driver (e.g. Chrome)
+driver = webdriver.Chrome()
+# Navigate to the website
+engine = pyttsx3.init()
+engine.setProperty("rate", 150)
+
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[2].id)  # this is female voice
+
+
+
+driver.get(url)
+hashOfComments = []
+k = 1
+
+while k < resetComment:
+    body = driver.find_elements(By.ID, "log")
+
+    for i in body:
+        comments = i.text.split('\n')
+        if comments == ['']:
+            comments = []
+        for comment in comments:
+            if hash(comment) not in hashOfComments:
+                hashOfComments.append(hash(comment))
+
+                ##GENERATE COMMNET
+                commentAuthor = comment.split(' ')[0] 
+                commentComment = comment.split(' ')[1:]
+                
+                commentString = ''
+                for words in commentComment:
+                    commentString = commentString + words + ''
+
+
+                finalComment = commentAuthor + '  Dice    ' + commentString
+                print(finalComment)
+                
+
+                engine.save_to_file(finalComment, finalComment + '.mp3')
+                engine.say(finalComment)
+                engine.runAndWait()
+
+                
+    time.sleep(1)
+    #reset comments
+    if k == (resetComment - 1):
+        hashOfComments = []
+        k = 0
+        driver.get(url)
+    k+=1
+
+
